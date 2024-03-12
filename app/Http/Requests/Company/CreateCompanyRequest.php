6dbd3643e7e363b8a57f92ca0dfbb01536220ffa
henrqueDev <lhdeveloper137@gmail.com>
2024-03-12 18:51:47 -0300

    fix - unecessary unique db bug + fix - users seed without env password #2

--- app/Http/Requests/Company/CreateCompanyRequest.php
+++ app/Http/Requests/Company/CreateCompanyRequest.php
@@ -22,7 +22,7 @@ class CreateCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|unique:companies|string|max:255'
+            'name' => 'required|string|max:255'
         ];
     }
 }
